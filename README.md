4bd46e2f3982a1395b217a22983deb747b9f8d5f
albevaldi <albevaldi1@gmail.com>
2024-05-07 22:36:35 -0400

    Get Requests for images, changed application format

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 Python application that generates a quote and image.
 
-Currently randomly picking images out of folder.
-
 Grabbing random quotes from quotable API.
+Grabbing random images from Unsplash API.
--- imageQuoteGenerator.py
+++ imageQuoteGenerator.py
@@ -11,35 +11,41 @@ height= root.winfo_screenheight()
 
 root.geometry("%dx%d" % (width, height))
 
-url = "https://api.quotable.io/random?size=1"
+quoteAPI = "https://api.quotable.io/random?size=1"
+imageAPI = "https://source.unsplash.com/random/1280x720"
 
-height -= 300
-width -= 100
 def generate():
+    rq = requests.get(quoteAPI)
+    quote = rq.json()
+    full_quote = "\"" + quote["content"] + "\""
 
-    print(height, width, "HEEERE")
-    r = requests.get(url)
-    quote = r.json()
-    full_quote = "\"" + quote["content"] + "\"" + " " + quote["author"]
-
-    image_path = "./Images/" + random.choice(os.listdir("./Images"))
-    image = Image.open(image_path)
-    image = image.resize((width, height))
+    img = requests.get(imageAPI)
+    file = open('image.jpg', 'wb')
+    file.write(img.content)
+    image = Image.open(r"image.jpg")
     image = ImageTk.PhotoImage(image)
+    file.close()
 
     image_label = tk.Label(root, image=image)
     image_label.grid(row=0, column=0, padx=5, pady=10)
     image_label.image = image
     quote_frame.grid(row=1, column=0, padx=5, pady=5)
     label.configure(text=full_quote)
+    label2.configure(text=quote["author"])
 
-quote_frame = tk.Frame(root, bg="white")
+quote_frame = tk.Frame(root)
 quote_frame.grid(row=1, column=0, padx=350, pady=250)
 
-label = tk.Label(quote_frame, text="Image Quote Generator", fg="blue", bg="white", wraplength=600, justify="center", font=("Helvetica", 18, "bold"))
+label = tk.Label(quote_frame, text="Placeholder Quote", fg="white", wraplength=900, justify="center", font=("Helvetica", 20, "bold italic"))
 label.pack()
 
+label2 = tk.Label(quote_frame, text="Placeholder Author", fg="white", wraplength=600, justify="center", font=("Roman", 18))
+label2.pack()
+
 button = tk.Button(root, text="Generate!", command=generate)
-button.grid(row=2, column=0, padx=5, pady=5, sticky="s")
+button.grid(row=0, column=1, padx=1, pady=5, sticky="nwe")
+
+# Launches application with randomly generated image and quote
+generate()
 
 root.mainloop()
